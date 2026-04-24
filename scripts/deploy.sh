07d258c80b6aee635c72b9f536112256b946d6e2
VyacheslavShtyrlin <georgesescoffierauguste@gmail.com>
2026-04-24 12:19:45 +0500

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -203,12 +203,13 @@ http {
         }
 
         location ${PANEL_PATH}/ {
-            proxy_pass         http://127.0.0.1:${PANEL_PORT}/;
-            proxy_http_version 1.1;
-            proxy_set_header   Host \$host;
-            proxy_set_header   X-Real-IP \$remote_addr;
-            proxy_set_header   Upgrade \$http_upgrade;
-            proxy_set_header   Connection "upgrade";
+            proxy_pass          https://127.0.0.1:${PANEL_PORT}/;
+            proxy_http_version  1.1;
+            proxy_ssl_verify    off;
+            proxy_set_header    Host \$host;
+            proxy_set_header    X-Real-IP \$remote_addr;
+            proxy_set_header    Upgrade \$http_upgrade;
+            proxy_set_header    Connection "upgrade";
         }
     }
 }
